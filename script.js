6aecb22f5b8a688aace0d06906fd42ba61292091
ashermenachem <ashermenachem0@gmail.com>
2024-08-11 11:39:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    const searchBar = document.getElementById('searchBar');
+    searchBar.addEventListener('input', () => {
+        // Add functionality for search bar here
+    });
+});
